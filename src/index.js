0d42bab045ee7246a8af267185d8299d49651ba6
Emily Menezes <estudosmenezes@gmail.com>
2022-10-04 20:04:05 -0300

    :sparkles: Completing the first requirement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +12,21 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+const onRequestTalkers = async () => {
+  const pathDirname = path.resolve(__dirname, 'talker.json');
+  const results = JSON.parse(await fs.readFile(pathDirname, 'utf-8'));
+  return results;
+};
+
+app.get('/talker', async (_request, response) => {
+  const results = await onRequestTalkers();
+
+  if (!results) { 
+    return response.status(HTTP_OK_STATUS).send([]);
+  } 
+  response.status(HTTP_OK_STATUS).json(results);
 });
 
 app.listen(PORT, () => {
